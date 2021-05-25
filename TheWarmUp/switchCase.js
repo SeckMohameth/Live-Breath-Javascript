3153ef31703f27ccce1637cafe5e373a3d8ed418
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2021-05-25 01:20:48 -0400

    switch statements

---
+++ TheWarmUp/switchCase.js
@@ -0,0 +1,18 @@
+
+function numberWhat(n){
+    switch(n) {
+        case 1:
+            console.log("Number 1"); 
+            break;
+        case 2:
+            console.log("Number 2"); 
+            break;
+        case 3:
+            console.log("Number 3"); 
+        default:
+            console.log("Bro, what is even that");
+    }
+
+}
+
+numberWhat(0);
