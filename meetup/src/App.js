b98d21246da1eff1647b59043ad3f107e347b5c3
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2021-06-16 00:09:17 -0400

    wrapper components

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -1,16 +1,15 @@
 import { Route, Switch } from 'react-router-dom'
 
 
-
+import Layout from './components/layout/Layout'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites'
-import MainNavigation from './components/layout/MainNavigation'
 
 function App(){
   return ( 
   <div>
-    <MainNavigation />
+    <Layout>
     <Switch>
       <Route path='/' exact={true}>
         <AllMeetupsPage />
@@ -22,6 +21,7 @@ function App(){
         <FavoritesPage/>
       </Route>
     </Switch>
+    </Layout>
   </div>
   );
 }
