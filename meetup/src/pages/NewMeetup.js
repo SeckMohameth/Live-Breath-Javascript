3f3c87881dd89ca5fd2d363be738c469598aa803
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2021-06-23 12:27:07 -0400

    created a form and firebase database

--- meetup/src/pages/NewMeetup.js
+++ meetup/src/pages/NewMeetup.js
@@ -1,7 +1,26 @@
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData){
+    fetch('https://jambar-2d2bb.firebaseio.com/meetups.json',
+    {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      history.replace('/')
+    });
+  }
 
     return <section>
       <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>;
   
   }
