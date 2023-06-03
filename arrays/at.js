a81d25d2b5315d593a44ff31ce7d43cbb80937ca
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-03 00:53:47 +0000

    added the at and fill methods

--- arrays/at.js
+++ arrays/at.js
@@ -1,3 +1,7 @@
+//returns the element at the specified index in the array.
+
+// take one argument. The index
+
 const num = [1, 2, 3, 4, 5, 6]
 
 const index = num.at(3)
---
+++ arrays/fill.js
@@ -0,0 +1,10 @@
+// this method fills all the elements in the array with a static value
+
+//  takes 3 arguments -> value to fill with, the start index, and the end index 
+
+
+const arr = ['apple', 'banna', 'orange']
+
+arr.fill('berry', 1, 2)
+
+console.log(arr)
