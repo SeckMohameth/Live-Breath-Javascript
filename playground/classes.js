4e9b175148bd680911fd4947e6edcf57d0fdd110
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-09 22:44:29 +0000

    worked with classes

---
+++ playground/classes.js
@@ -0,0 +1,15 @@
+// creating a class
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}  
+
+
+const Mo = new Person("Mohameth", 26)
+const Dom = new Person("Dominic", 24)
+
+console.log(Person)
+console.log(Mo, Dom)
