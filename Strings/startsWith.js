6986ee1e4670adc8bab870b49ef5e328f680847a
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-10 23:34:18 +0000

    working with subString and startsWith methods

---
+++ Strings/startsWith.js
@@ -0,0 +1,5 @@
+let str = "Starts at the beginning"
+
+console.log(str.startsWith("Mo")) // returns false
+
+console.log(str.startsWith("Starts")) // returns true
---
+++ Strings/substring.js
@@ -0,0 +1,6 @@
+let str = "BrainStation"
+
+
+console.log(str.substring(0, 5)) // prints brain
+
+console.log(str.substring(5)) // prints station and the rest 
