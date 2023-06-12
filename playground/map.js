21d305849d056e073f4f270463eb57f131b7259c
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-12 14:43:33 +0000

    learning about map

---
+++ playground/map.js
@@ -0,0 +1,13 @@
+// map
+
+const score = new Map([
+  ["Mo", "33433"],
+  ["Dom", "4654"]
+])
+
+console.log(score)
+
+score.set("Kay", "90878") // add another 
+console.log(score)
+
+console.log(score.get("Mo")) // geting the value for key "Mo"
