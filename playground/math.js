292def1af18b671b46edec11b0b8fe79fc49cdbf
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-13 16:42:25 +0000

    learning about the Math object

---
+++ playground/math.js
@@ -0,0 +1,17 @@
+let num = 2.43433
+
+
+//Returns the greatest integer less than or equal to its numeric argument.
+console.log(Math.floor(num))
+
+//Returns a pseudorandom number between 0 and 1.
+console.log(Math.random())
+
+
+// number between 0 and 100
+console.log(Math.random() * 100)
+
+//whole number between 0 and 100
+let randomNum = Math.random() * 100
+console.log(Math.floor(randomNum))
+
