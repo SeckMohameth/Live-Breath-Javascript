46fe8c23c94d5a3190567aedbb1d2246d5840dae
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-18 00:18:57 +0000

    checking if lowercase and testing the alphabet with the ASCII table

---
+++ playground/ASCII.js
@@ -0,0 +1,22 @@
+// capital letters are lower on the ASCII table 
+// examples below
+
+if('A' >= 'd') {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+// false because a is 97 and d is 100 on the table
+if('a' >= 'd') {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+// will return true
+if('x' >= 'd') {
+  console.log(true)
+} else {
+  console.log(false)
+}
---
+++ playground/isLowercase.js
@@ -0,0 +1,11 @@
+function isLowerCase(string) {
+
+  console.log(string[0] >= 'a' && string[0] <= 'z'? true : false);
+}
+
+
+isLowerCase("Pillow")
+isLowerCase("pillow")
+
+
+
