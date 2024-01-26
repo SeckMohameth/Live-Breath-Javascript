b5835de7b44841ffd0d9f3e6dc91ee86781d9973
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-01-26 21:57:32 +0000

    working with objects and keys

---
+++ Objects/obejcst&keys.js
@@ -0,0 +1,15 @@
+let arr = ["banna", "apple", "orange"];
+
+let keys = Object.keys(arr)
+
+console.log(keys)
+
+console.log(Object.values(arr))
+
+let numbers = [1, 2, 3, 4, 5];
+
+let mo = {}
+
+mo.numbers[0] = numbers[0] * 2
+
+console.log(mo)
---
+++ Strings/playground.js
@@ -0,0 +1,14 @@
+
+//length of a string
+let firstName = "John";
+console.log(firstName.length)
+
+//lowercase
+console.log(firstName.toLowerCase())
+
+//uppercase
+console.log(firstName.toUpperCase())
+
+// Bracket notation for strings
+console.log(firstName[2])
+
