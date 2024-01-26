ca1c86f827dcaed227922af3884e88c2d8fc4142
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-01-26 15:58:10 +0000

    quick warm up from the CSX course. I'm going to finish JavaScript on that platform instead

---
+++ CodeSmith/precourse.js
@@ -0,0 +1,49 @@
+console.log("Hello Mo");
+
+let age = 26
+
+console.log(typeof age)
+
+const firstName = "Mohameth"
+
+isLoggedIn = true
+
+if(isLoggedIn) {
+  console.log("is logged in")
+}
+
+console.log("My name is " + firstName + " I am " + age + " years old ") 
+
+
+
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+
+console.table(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.push(7)
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+arr.unshift(0)
+console.log(arr)
+
+
+// ADD CODE HERE
+const name = "Mohameth"
+//let age = 26
+const loveJavaScript = true
+let listOfComplaints = null
+
+
+//Uncomment these to check your work!
+console.log('My name is ' + name);
+console.log('I am ' + age + ' years old');
+console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
+console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
