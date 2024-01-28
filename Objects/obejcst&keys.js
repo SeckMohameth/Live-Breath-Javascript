0388ac502ef8216d9c264baf0cf2d8b18223449b
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-01-28 02:12:57 +0000

    appending to objects from an array

--- Objects/obejcst&keys.js
+++ Objects/obejcst&keys.js
@@ -12,4 +12,13 @@ let mo = {}
 
 // mo.numbers[0] = numbers[0] * 2
 
+
+// mo.name = "Mohameth"
+
+let restaurent = ["Burger", "Pizza", "Pasta", "Salad"]
+
+for(let i = 0; i < restaurent.length; i++) {
+  mo.restaurent[i] = restaurent[i] 
+}
+
 console.log(mo)
---
+++ Objects/playground.js
@@ -0,0 +1,8 @@
+let food = ["cake", "pizza", "ice cream", "pie"];
+let favorites = {}
+
+for (let i = 0; i < food.length; i++) {
+  favorites[i] = food[i];
+}
+
+console.log(favorites);
