819af762503bf4197b40075054278e36f0f5f12e
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-02-01 22:47:33 +0000

    learned about different string methods

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let words = "This is the beginning of something new and amazing"
 
 let name = "Mohameth"
 
+let greeting = "Hello, Mohameth!"
+
 let Person = {
   name: "Mo",
   age: 26,
   job: "Software Engineer",
   HomeTown: "West Haven"
 }
-
-let fruits = ["apple", "orange", "banana", "pear", "watermelon", "pineapple", "Black Berry"]
 
 let songs = {
   artist: "",
@@ -18,14 +18,68 @@ let songs = {
   year: 0
 }
 
+//========================================================
+
+// lowercase
+let lowerCase = name.toLowerCase()
+console.log(lowerCase)
+
+// uppercase
+let upperCase = name.toUpperCase()
+console.log(upperCase)
+
+//index of
+console.log(name.indexOf("t"))
+
+//last index of something
+console.log(name.lastIndexOf("h"))
+
+//index of
+console.log("The index of 6 in numbers array is " + numbers.indexOf(6))
+
+//char at
+console.log(greeting.charAt(4))
+
+
+//reversing a string
+console.log(name.split("").reverse().join(""))
+
+// replac a string
+console.log(greeting.replace("Hello", "Bye"))
+
+//extract a portion of an string
+console.log(name.slice(0, 5))
+
+
+//Interpolation
+console.log(`Hello agian, ${name}`)
+
+//Concatenation
+console.log("What's up " + name)
+
+//trimming string
+console.log(words.trim())
+
+// trimStart and trimEnd are other ways to get rid of white space
+
+//length
+console.log(name.length)
+//======================= Arrays =========================
+
+let fruits = ["apple", "orange", "banana", "pear", "watermelon", "pineapple", "Black Berry"]
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let randomNum = [23, 4, 5, 6, 2, 2, 34, 16, 80]
+
+let months = ["January, March", "April", "October", "September", "December", "July", "May", "June"]
+
+let firends = ["Dom", "chris"]
 
 
 
 
-
-
-
-
+//========================================================
 
 
 //fundamentals sir
