57d8d8341a3d3a6db0f5ccb09db24bb6242b8e93
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-02-11 13:35:09 +0000

    checking for the next element

---
+++ arrays/checkElements.js
@@ -0,0 +1,11 @@
+let num = [1, 2, 3, 4, 5, 6]
+
+for(let i = 0; i < num.length; i++) {
+  console.log(num[i])
+
+  console.log(num[i + 2])
+}
+
+
+// you check for the next element like this - array[i + 1] or array[i + 2] and so on
+
